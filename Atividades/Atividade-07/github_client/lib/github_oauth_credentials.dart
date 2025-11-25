23bc91e374682370defe8f78670307b59f77f13d
Kragdom <gustavofs0711@gmail.com>
2025-11-25 16:32:54 -0300

    Atividade-07 feita adequadamente

---
+++ Atividades/Atividade-07/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23li6F3xTbKJDlswpV';
+const githubClientSecret = '18d618413f0b78df97c1e880f4053f4e8af11ff3';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
